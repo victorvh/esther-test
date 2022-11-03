dd2d6495fb13ebb08994ecce7e6b5c136bc93579
VictorVH <victor@skavi.dk>
2022-11-03 11:11:00 +0100

    listen to port 8080

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ const server = http.createServer((req, res) => {
   res.end(JSON.stringify(JSON.parse(newData as unknown as string)));
 });
 
-server.listen(3000, () => {
+server.listen(80, () => {
   console.log("Server is listening on port 3000");
 });
